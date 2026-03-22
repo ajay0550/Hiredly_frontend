4634679d4bedc134186aa0c3875590dc78cc4c6d
ajay0550 <ajaygundu03@gmail.com>
2026-03-22 11:21:36 +0530

    connected to the backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hiredly-backend-6brj.onrender.com",
 });
 
 // Attach token automatically
